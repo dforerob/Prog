1bd3cccb6fa756799ab225ede00d88ac52a9d70b
Diego Alejandro Forero Benitez <dforerob@darkstar.org>
2019-09-06 08:50:37 -0500

    Programa leer

---
+++ leer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(void)
+
+{
+  double x=0.0;
+  std::cout << "Escribe el numero:" << std::endl;
+  std:: cin >> x;
+  std:: cout << "He Leido: " << x <<  std::endl;
+  
+  return 0;
+
+}
+1

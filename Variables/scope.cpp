f49fd428dde656e8fe27b7a1af730bd24428140d
Diego Alejandro Forero Benitez <dforerob@darkstar.org>
2019-09-11 09:16:59 -0500

    trabajo de variable 1t

---
+++ Variables/scope.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+int main(void)
+{
+ double miles = 0.0;
+ double km = 0.0;
+ std:: cout <<"escribe el numero de millas :\n";
+ std:: cin >>miles;  
+ km = miles*1.609;
+    std::cout << km <<"\n";
+       
+  return 0;
+
+}
+  
